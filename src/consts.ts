810fca8a58e459ccb3f45a40fed5409ac983efc7
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2024-12-21 11:16:43 -0500

    fix: :art: Page title fixes

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Anders Ramsay";
-export const SITE_DESCRIPTION = "Designing and Building Digital Products";
+export const SITE_TITLE =
+  "Anders Ramsay - Designing and Building Digital Products";
