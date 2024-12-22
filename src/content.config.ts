685051b550c8d491b91fb9ee3f7f1f671428e6a7
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2024-12-22 13:28:37 -0500

    feat: :sparkles: Display image credits with optional link

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,7 +13,10 @@ const blog = defineCollection({
       toc: z.boolean().optional(),
       pubDate: z.coerce.date(),
       updatedDate: z.coerce.date().optional(),
-      heroImage: z.tuple([image(), z.string()]).optional(),
+      heroImage: z.tuple([image(), z.string()]).optional(), // [image, alt]
+      heroImageCredit: z
+        .tuple([z.string(), z.union([z.literal(""), z.string().trim().url()])])
+        .optional(), // [credit name, credit link OR ""]
     }),
 });
 
